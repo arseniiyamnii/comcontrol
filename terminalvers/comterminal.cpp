0b430c602d5e17f1a2008505cb5bb2d612fcdc9b
arseniiyamnii <yamnii@ya.ru>
2017-12-15 03:32:30 +0500

    withterminalWork!(only in visualstudio)

---
+++ terminalvers/comterminal.cpp
@@ -0,0 +1,20 @@
+// arducom.cpp: главный файл проекта.
+//работает только в visual c++
+#include "stdafx.h"
+using namespace System;
+using namespace System::IO::Ports;
+int main(array<System::String ^> ^args)
+{
+	SerialPort port("COM4", 9600);
+	port.Open();
+	while (true){
+		int input = Convert::ToInt32(Console::ReadLine());
+		if (input == 1)
+		{
+			port.Write("1");
+		}
+		else 
+			port.Write("2");
+	}
+    return 0;
+}
